328c8d345f6d2ab7a4a3afe1aabd763b6a5301d0
Mehi <mahammad.akhundlu@gmail.com>
2026-08-12 16:13:14 +0400

    feat: complete complete simple local runner supporting AC,WA,TLE,RE AND CE

--- runner.py
+++ runner.py
@@ -1,7 +1,20 @@
 import subprocess
 
 compile_result = subprocess.run(["g++", "solution.cpp", "-o", "solution"], capture_output=True, text= True)
+
 if compile_result.returncode == 0:
-    print("Success!")
-    run_result = subprocess.run(["./solution"], input="5 10\n", capture_output= True, text= True)
-print(run_result.stdout)
+    try:
+        run_result = subprocess.run(["./solution"], input="5 10\n",timeout = 2.0, capture_output= True, text= True)
+        if run_result.returncode == 0:
+            if run_result.stdout.strip() == "15":
+                print(run_result.stdout,"\n","ACCEPTED1")
+            else:
+                print("Wrong Answer")
+        else:
+            print("RUNTIME ERROR!")
+
+    except subprocess.TimeoutExpired:
+        print("TLE!")
+else:
+    print("Error!")
+    print(compile_result.stderr)
--- solution.cpp
+++ solution.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n,m;
     cin>>n>>m;
-    cout<<n+m<<"\n";
-    
+    cout<<n+m+1<<"\n";
+
     return 0;
 }
